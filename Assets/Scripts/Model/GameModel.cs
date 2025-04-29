283a1a8a84ad1de6a7e5842f445f8ab3d4fcee6b
evagr11 <evagallardor@icloud.com>
2025-04-29 18:39:41 +0200

    Creación de carpetas y GameModel

---
+++ Assets/Scripts/Model/GameModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModel
+{
+    // Aquí más adelante: tablero, piezas, turnos, cursor...
+
+    public GameModel()
+    {
+        // Inicializaciones que vendrán en los siguientes días
+    }
+}
